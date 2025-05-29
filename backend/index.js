8af70e76bb0cd869441037ab65ec6eacec7f4dab
Sai Prashanth <saiprashanth381@gmail.com>
2025-05-29 17:23:15 +0530

    cors changes

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-28dd.onrender.com',
     credentials:true
 }))
 
